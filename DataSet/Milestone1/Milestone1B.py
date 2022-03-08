a8679f2fdbc9fcbe3f99608d4bea4dd18b31d6c3
015amit <ak2383977@gmail.com>
2022-03-08 11:16:42 +0530

    first commit

---
+++ DataSet/Milestone1/Milestone1B.py
@@ -0,0 +1,58 @@
+from importlib.abc import Loader
+import yaml
+from threading import *
+import datetime
+from yaml import Loader
+
+def entry_log(k):
+    with open("Milestone1B_log.txt", 'a') as write_log:
+        t = datetime.datetime.now()
+        write_log.write(str(t) + " " + str(k))
+        write_log.write('\n')
+        write_log.close()
+        
+
+def load_data(path):
+    with open(path, 'r') as file_read:
+        data = yaml.load(file_read, Loader=Loader)
+        return data
+    
+file = "DataSet/Milestone1/Milestone1B.yaml"
+
+data = load_data(file)
+data = data['M1B_Workflow']
+
+txt = "M1B_Workflow"
+
+tasks = []
+
+def find_task(txt,data):
+    entry_log(txt + " Entry ")
+    if data['Type'] == 'Flow':
+        execution = data['Execution']
+        activities = data['Activities']
+        if execution == 'Sequential':
+            for k, v in activities.items():
+                find_task(txt + "." + k ,v)
+        elif execution == 'Concurrent':
+            thread_items = []
+            for k, v in activities.items():
+                t = Thread(target=find_task, args=(txt + "." + k ,v,))
+                thread_items.append(t)
+            for t in thread_items:
+                t.start()
+            for t in thread_items:
+                t.join()
+    elif data['Type'] == 'Task':
+        func = data['Function']
+        finput = data['Inputs']['FunctionInput']
+        ftime = data['Inputs']['ExecutionTime']
+        entry_log(txt + " Executing " + str(func) + " (" + str(finput) + str(ftime) + " ) ")
+        tasks.append(data)
+    entry_log(txt + " Exit")
+
+find_task(txt, data)
+
+print(tasks)
+        
+        
